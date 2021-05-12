5407028ace90c934cb23e8c52b89129871e2dc98
nayanika2304 <nayanikabhargava1993@gmail.com>
2021-05-12 16:50:35 -0400

    changes

--- src/index.js
+++ src/index.js
@@ -57,7 +57,5 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-export const history = createBrowserHistory({
-    basename: process.env.REACT_APP_PUBLIC_URL
-});
+
 reportWebVitals();
