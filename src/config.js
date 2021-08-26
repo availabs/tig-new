185210f918560827da1c139ae01ae951a65f9d95
shaunaksangdod <shaunaksangdod@gmail.com>
2021-08-26 15:33:11 -0400

    hosting setup

--- src/config.js
+++ src/config.js
@@ -5,8 +5,8 @@ let AUTH_HOST = 'https://availauth.availabs.org';
 let CLIENT_HOST = 'https://npmrds.availabs.org';
 
 if (process.env.NODE_ENV === 'development') {
-  API_HOST = 'http://localhost:4444';
-  AUTH_HOST = 'http://localhost:3457';
+  // API_HOST = 'http://localhost:4444';
+  // AUTH_HOST = 'http://localhost:3457';
   CLIENT_HOST = 'http://localhost:3000';
 }
 export { API_HOST, AUTH_HOST, CLIENT_HOST };
